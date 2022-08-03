a3fd3ae963171aa262205cd79f97b746fbb2548e
Adel Kouaou <adel.kouaou@hotmail.com>
2022-08-03 07:49:25 -0400

    REVERTING

--- server.js
+++
@@ -1,9 +0,0 @@
-let express = require("express");
-let app = express();
-app.use(express.static(__dirname + "/dist/garderie.client"));
-
-app.get("/*", (req, res) => {
-  res.sendFile(__dirname + "/dist/garderie.client/index.html");
-});
-
-app.listen(process.env.PORT || 8080);
